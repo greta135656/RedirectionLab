b68ec035f275c901e7d11fa32248aa2e1111ee84
greta135656 <72942679+greta135656@users.noreply.github.com>
2021-01-05 18:24:52 +0000

    accidentally deleted repo...

---
+++ MakeStars.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class MakeStars {
+  public static void main (String[] args){
+    Scanner result = new Scanner(System.in);
+    while (result.hasNextLine()){
+      Scanner line = new Scanner(result.nextLine());
+      String out = "";
+    while (line.hasNext()){
+      String word = line.next();
+      for (int i=0; i<word.length(); i++){
+        out += "*";
+        if(line.hasNext()) {
+     out += "";
+      }
+    }
+    System.out.println(out);
+    
+  }}}}
