2085d9964f16ec9d13fd272b297a87828f188673
greta135656 <72942679+greta135656@users.noreply.github.com>
2021-01-05 18:25:46 +0000

    created file

---
+++ PigLatin.java
@@ -0,0 +1,11 @@
+//PigLatin.java
+import java.util.*;
+public class PigLatin {
+  public static void main(String[]args){
+public static String pigLatinSimple(String s){}
+
+
+  public static String pigLatin(String s){}
+
+
+    public static String pigLatinBest(String s){}}}
